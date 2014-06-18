c1081976a3aca0ac6c16cd0cdcc2587cf64263d0
crich303 <charlesrichardson@anderson5.net>
2014-06-18 09:24:49 -0400

    Player Changes
    Changed player

--- 1.3.9.P.PY prisoners_dilemma.py
+++ 1.3.9.P.PY prisoners_dilemma.py
@@ -226,16 +226,16 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
     #
     elif player == 5:
         if getting_team_name:
-            return 'loyal vengeful'
+            return 'super MacDaddy'
         else:
             # use history, opponent_history, score, opponent_score
             # to compute your strategy
-            if len(opponent_history)==0: #It's the first round: collude
-                return 'c'
-            elif history[-1]=='c' and opponent_history[-1]=='b':
-                return 'b' # betray is they were severely punished last time
+            if len(opponent_history)==0: #It's the first round: betray
+                return 'b'
+            elif history[-1]=='b' and opponent_history[-1]=='b':
+                return 'b' # betray if they were severely punished last time
             else:
-                return 'c' #otherwise collude
+                return 'b' #otherwise betray
     
     
     
@@ -559,7 +559,6 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
                 return 'c' #otherwise collude
     
     
-
 
 
 
